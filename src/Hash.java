5e8b917f0e7ce0264647a63f823704764ac8de87
agastyabrahmbhatt <agastyabrahmbhatt@menloschool.org>
2024-10-26 17:17:27 -0700

    code

--- src/Hash.java
+++ src/Hash.java
@@ -30,63 +30,68 @@ public class Hash {
         return invalidSTR;
     }
 
-    //
+    // Initialized to the finder invalid string
     public void setInvalidSTR(String invalidSTR) {
         this.invalidSTR = invalidSTR;
     }
 
-    // Converts to hash
-        public int hashFunction(String key){
-            long h = 0;
-            int index = 0;
-            for(int i = 0; i < key.length(); i++){
-                h = (h * R + key.charAt(i)) % p;
-            }
-            /*
-            Below code from the book Algorithms Sedgewick & Wayne, pg. 461.
-             */
-            index = (int) ((h & 0x7fffffff) % bucket);
-            //System.out.println("Index: " + index + " Key " + key);
-            return index;
+    /* Using the horner's hash method for strings
+        Revise the return code to be % bucket size so
+        the hash code is converted to the index for the hash table.
+     */
+    public int hashFunction(String key){
+        long h = 0;
+        int index = 0;
+        for(int i = 0; i < key.length(); i++){
+            h = (h * R + key.charAt(i)) % p;
         }
+        /*
+           Below code from the book Algorithms Sedgewick & Wayne, pg. 461.
+        */
+        index = (int) ((h & 0x7fffffff) % bucket);
+        //System.out.println("Index: " + index + " Key " + key);
+        return index;
+    }
 
-        public void insertItem(String key, String value)
-        {
-            // get the hash index of key
-            int index = hashFunction(key);
+    // Inserts the data pair object with key values into the hash table.
+    public void insertItem(String key, String value)
+    {
+        // get the hash index of key
+        int index = hashFunction(key);
 
-            if(table[index] == null){
-                table[index] = new ArrayList<DataPair>();
-            }
-            // insert key into hash table at that index
-            table[index].add(new DataPair(key, value));
+        if(table[index] == null){
+            table[index] = new ArrayList<DataPair>();
         }
+        // insert key into hash table at that index
+        table[index].add(new DataPair(key, value));
+    }
 
-        public String getValue(String key){
-            DataPair pPair;
-            int index = hashFunction(key);
-            if(table[index] == null){
-                return getInvalidSTR();
-            }
-            for(int i = 0; i < table[index].size(); i++){
-                pPair = table[index].get(i);
-                if(key.equals(pPair.getKey())){
-                    return pPair.getVal();
-                }
-            }
+    // returns the value for a given string from the hashtable.
+    public String getValue(String key){
+        DataPair pPair;
+        int index = hashFunction(key);
+        if(table[index] == null){
             return getInvalidSTR();
         }
-
-        // function to display hash table
-        // Used for debugging
-        public void displayHash()
-        {
-            for (int i = 0; i < bucket; i++) {
-                System.out.print(i);
-                for (DataPair x : table[i]) {
-                    System.out.print(" --> " + x.getKey() + ' ' + x.getVal());
-                }
-                System.out.println();
+        for(int i = 0; i < table[index].size(); i++){
+            pPair = table[index].get(i);
+            if(key.equals(pPair.getKey())){
+                return pPair.getVal();
             }
         }
+        return getInvalidSTR();
+    }
+
+    // function to display hash table
+    // Used for debugging
+    public void displayHash()
+    {
+        for (int i = 0; i < bucket; i++) {
+            System.out.print(i);
+            for (DataPair x : table[i]) {
+                System.out.print(" --> " + x.getKey() + ' ' + x.getVal());
+            }
+            System.out.println();
+        }
+    }
 }
